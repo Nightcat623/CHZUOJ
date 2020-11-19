10c806055b2e5e2321f25f468609236ab6885fe8
Nightcat623 <74492938+Nightcat623@users.noreply.github.com>
2020-11-19 21:06:55 +0800

    Update and rename 1133error to 1133.cpp

--- 1133.cpp
+++ 1133.cpp
@@ -1,6 +1,8 @@
 #include<stdio.h>
+int s=1;//定义全局变量以对应步数改变 
 int jump(int n,char x, char y){
-	printf("第步:将%d号盘子从%c--->%c\n",n,x,y);
+	printf("第%d步:将%d号盘子从%c--->%c\n",s,n,x,y);
+	s++;
 }
 void step(int n,char x,char y,char z){
 	if(n==1) jump(n,x,z);
