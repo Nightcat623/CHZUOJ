ec70af649bdc1ef252be6f723d451471f8fe7bf8
Nightcat623 <74492938+Nightcat623@users.noreply.github.com>
2020-11-19 19:52:04 +0800

    Create 1118.cpp

---
+++ 1118.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int f( int n );
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%d\n", f(n));
+	return 0;
+}
+int f( int n ){
+	if(n==1) return 1;
+	else if(n==2) return 2;
+	else return f(n-1)+f(n-2) ;
+}
