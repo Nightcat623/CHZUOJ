64a6d242bcb2513466f348e8d522385dc01ece17
Nightcat623 <74492938+Nightcat623@users.noreply.github.com>
2020-11-26 20:42:12 +0800

    Update 1146.cpp

--- 1146.cpp
+++ 1146.cpp
@@ -1,12 +1,13 @@
-#include<stdio.h> 
+#include<stdio.h>
 int main(){
-	int n,i;
-	scanf("%d\n",&n);
-	int a[n];
-	for(i=n;i>0;--i)
-	scanf("%d ",&a[i]);
-	for(i=1;i<=n;i++){
-		printf("%d ",a[i]);
-	} 
-	return 0;
-}//使用ctrl+z停止程序 
+  int a[11], i, n;
+  scanf("%d", &n);
+  for(i = 1; i <= n; i++){   
+    scanf("%d", &a[i]);
+  }
+  for(i = n; i >= 1; i--){
+    printf("%d ", a[i]);
+
+  }
+  return 0;
+}
